7c5ac09634bf0e519bb71ace4ceb6a3830349eb2
andriylyseiko <andriylyseiko@gmail.com>
2024-09-10 12:41:59 +0300

    Added keaypad for user input

--- Core/Inc/stm32f4xx_it.h
+++ Core/Inc/stm32f4xx_it.h
@@ -56,6 +56,7 @@ void ADC_IRQHandler(void);
 void I2C1_EV_IRQHandler(void);
 void SPI1_IRQHandler(void);
 void USART2_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
 void TIM6_DAC_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
